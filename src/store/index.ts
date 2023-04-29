f91594b61403b3e417e385d2b3c9813ac3bb6bab
Yahor Litavar <sergioegor21@gmail.com>
2023-04-29 22:22:03 +0300

    feat: add docs, fix persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -22,6 +22,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ['top']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
