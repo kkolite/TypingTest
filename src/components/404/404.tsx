ff240b1ee1c742f2bc94e1b0490533f4c90e8526
Yahor Litavar <sergioegor21@gmail.com>
2023-05-03 11:10:30 +0300

    fix: error page button

--- src/components/404/404.tsx
+++ src/components/404/404.tsx
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
 import { TEXT } from "../../data/text";
 import styles from "./404.module.scss";
+import { EPages } from "../../data/types";
 
 export const ErrorPage = () => {
   return (
     <div className={styles.container}>
       <p className={styles.h}>{TEXT.ERROR.HEADER}</p>
       <p className={styles.text}>{TEXT.ERROR.MAIN}</p>
-      <button className={styles.button + " button"}>{TEXT.ERROR.BUTTON}</button>
+      <NavLink to={EPages.MAIN}>
+        <button className={styles.button + " button"}>{TEXT.ERROR.BUTTON}</button>
+      </NavLink>
     </div>
   );
 };
